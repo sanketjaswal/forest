4dafb554bcf55dbfdf4387a21332a3a7f9cacc1e
sanketjaswal <sanket.jaswal123@gmail.com>
2021-10-26 17:43:00 +0530

    page content added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Nav } from "./Components/Home/Nav";
+import { PageContent } from "./Components/Home/PageContent";
 
 function App() {
   return (
     <div className="main">
       <Nav />
+      <PageContent />
     </div>
   );
 }
---
+++ src/Components/Home/PageContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./PageContent.css";
+
+export const PageContent = () => {
+  return (
+    <div className="PageContentContainer">
+      <div className=""></div>
+    </div>
+  );
+};
